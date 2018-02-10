10afd56a97ed677e123cf596de4eb5c826319bd1
uhudpoyraz <uhud.poyraz@gmail.com>
2018-02-10 21:19:49 +0300

    film list and film detail service were created

--- src/app/providers/film.service.ts
+++ src/app/providers/film.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class FilmService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getFilmList() {
+    return this.http.get('http://www.omdbapi.com/?s=matrix&apikey=5fc4b314');
+  }
+
+  public details(id: string){
+    return this.http.get('http://www.omdbapi.com/?i=' + id + '&apikey=5fc4b314');
+  }
 
 }

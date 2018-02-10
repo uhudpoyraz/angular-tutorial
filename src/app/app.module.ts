c1b4426d69960a3fc39d6eb87e59eb0f3f728304
uhudpoyraz <uhud.poyraz@gmail.com>
2018-02-10 21:07:43 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilmDetailComponent } from './film-detail/film-detail.component';
+import { FilmListComponent } from './film-list/film-list.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilmDetailComponent,
+    FilmListComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule
